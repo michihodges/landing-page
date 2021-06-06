2c3b823e4742e1642165ce48562a547dba8c3b47
michihodges <michihodges@gmail.com>
2021-06-06 23:34:12 +0100

    feat: Nav
    Added styling to nav as well as playing with scrollTo options.

--- js/app.js
+++ js/app.js
@@ -40,11 +40,27 @@ const sections = document.querySelectorAll('section'); // array of sections
 function buildNav() { // Function declaration
     for (let i = 0; i < sections.length; i++) { // for loop itterates over the length of all sections
         let navElement = document.createElement("li"); // Creates a new list element
+        navElement.classList.add("menu__link");
+        // navElement.addEventListener("click", onNavElementCLick);
         navbar.appendChild(navElement); // Adds new list element to the navbar
         let navSection = sections[i]; // Selects a section out of the sections array
         let navSelection = navSection.dataset.nav; // Identifies each data-nav attribute in each section with dataset.nav
         console.log(navSelection); // Prints each data-nav attribute to the console
         navElement.textContent = navSelection; // Adds data-nav attribute to the nav
+        // navElement.onclick = function (){
+        //     navSection.scrollIntoView(); //// scrollTo (where? how to modify?)
+        // }
+        // navElement.onclick = function (){
+        //     console.log("Listening");
+        // }
+        // navElement.addEventListener("click", () => {
+        //     navSection.scrollIntoView();
+        // })
+        // navElement.addEventListener("click", () => {
+        //     console.log("Listening")
+        // })
+
+        //// how to use the function not inline and outside of scope?
     }
 }
 
@@ -52,7 +68,16 @@ function buildNav() { // Function declaration
 
 
 // Scroll to anchor ID using scrollTO event
-
+// function onNavElementCLick(mouseEvent) {
+//     console.log(this);
+//     window.scrollTo(0, 100);
+// }
+function scrollToClick() {
+    navbar.addEventListener('click', function (event) {
+        const clicked = document.querySelector('#' + event.target.dataset.nav)
+        clicked.scrollIntoView();
+    });
+};
 
 /**
  * End Main Functions
@@ -64,5 +89,6 @@ function buildNav() { // Function declaration
 buildNav(); // Calls function
 
 // Scroll to section on link click
+scrollToClick();
 
 // Set sections as active
