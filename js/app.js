03ab561369cb3c1de39f247fa928bc67c32090d6
michihodges <michihodges@gmail.com>
2021-06-22 23:08:38 +0100

    refactor: scrollTo
    Deleted code examples no longer needed.

--- js/app.js
+++ js/app.js
@@ -41,7 +41,6 @@ function buildNav() {
     for (let i = 0; i < sections.length; i++) {
         let navElement = document.createElement("li");
         navElement.classList.add("menu__link");
-        // navElement.addEventListener("click", onNavElementCLick);
         navbar.appendChild(navElement);
         let navSection = sections[i]; // Selects a section out of the sections array
         let navSelection = navSection.dataset.nav; // Identifies each data-nav attribute in each section with dataset.nav
@@ -67,16 +66,6 @@ function scrollToAnchor(event) {
     });
     console.log("Frasier Crane is listening.");
 }
-// function onNavElementCLick(mouseEvent) {
-//     console.log(this);
-//     window.scrollTo(0, 100);
-// }
-// function scrollToClick() {
-//     navbar.addEventListener('click', function (event) {
-//         const clicked = document.querySelector('#' + event.target.dataset.nav)
-//         clicked.scrollIntoView();
-//     });
-// };
 
 /**
  * End Main Functions
@@ -88,7 +77,5 @@ function scrollToAnchor(event) {
 buildNav(); // Calls function
 
 // Scroll to section on link click
-// scrollToClick();
-// scrollToElement();
 
 // Set sections as active
